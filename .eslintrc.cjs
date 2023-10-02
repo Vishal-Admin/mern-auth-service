83ac1abd8077f07af9c64ad45950929a14faa811
VishalPanchal <vishalpanchal570@gmail.com>
2023-10-02 06:35:15 +0530

    test husky2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,6 @@ module.exports = {
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended-type-checked",
-        // "prettier",
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
